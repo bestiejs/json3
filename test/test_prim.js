94edfa71ffab0bb03677229faec6d12781e7e0d6
Kit Cambridge <kitcambridge@me.com>
2012-02-16 20:19:56 -0700

    According to ES 5.1 section 15.12.1.1, unescaped tab characters are not permitted within strings.

--- test/test_prim.js
+++ test/test_prim.js
@@ -104,7 +104,7 @@ exports["Test String Literals"] = function () {
   exports.parseError('"\\x61"', "String containing a hex escape sequence");
   exports.parseError('"hello \r\n world"', "String containing an unescaped CRLF line ending");
   ["\u0000", "\u0001", "\u0002", "\u0003", "\u0004", "\u0005", "\u0006",
-    "\u0007", "\b", "\n", "\u000b", "\f", "\r", "\u000e", "\u000f",
+    "\u0007", "\b", "\t", "\n", "\u000b", "\f", "\r", "\u000e", "\u000f",
     "\u0010", "\u0011", "\u0012", "\u0013", "\u0014", "\u0015", "\u0016",
     "\u0017", "\u0018", "\u0019", "\u001a", "\u001b", "\u001c", "\u001d",
     "\u001e", "\u001f"].forEach(function (value) {
