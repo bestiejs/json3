a81d5ee670aa645a7a2e16171ae6c48a94c0b4cd
Kit Cambridge <kitcambridge@me.com>
2012-02-18 13:15:13 -0700

    Add conformance tests from Microsoft's ECMAScript 5 Conformance Suite.

--- test/test_prim.js
+++ test/test_prim.js
@@ -13,9 +13,14 @@ exports.parseError = function (source, message, callback) {
   }, /SyntaxError/, message);
 };
 
-// Ensure that Prim parses the given source string correctly.
+// Ensures that Prim parses the given source string correctly.
 exports.parses = function (expected, source, message, callback) {
   assert.ok(_.isEqual(expected, prim.parse(source, callback)), message);
+};
+
+// Ensures that Prim serializes the given object correctly.
+exports.serializes = function (expected, value, message, filter, width) {
+  assert.strictEqual(expected, prim.stringify(value, filter, width), message);
 };
 
 exports["Test Empty Source String"] = function () {
@@ -156,7 +161,230 @@ exports["Test Serialization"] = function () {
   assert.equal(prim.stringify([1], null, "  "), "[\n  1\n]", "Single-element array; optional `whitespace` argument");
   assert.equal(prim.stringify({"foo": {"bar": [123]}}), "{\"foo\":{\"bar\":[123]}}", "Nested objects");
   assert.equal(prim.stringify({"foo": 123}, null, "  "), "{\n  \"foo\": 123\n}", "Single-member object; optional `whitespace` argument");
-  assert.equal(prim.stringify({"foo": 123, "bar": 456}, ["bar"], "  "), "{\n  \"bar\": 456\n}", "Object; optional `select` and `whitespace` arguments");
+  assert.equal(prim.stringify({"foo": 123, "bar": 456}, ["bar"], "  "), "{\n  \"bar\": 456\n}", "Object; optional `filter` and `whitespace` arguments");
+};
+
+/*
+ * The following tests are adapted from the ECMAScript 5 Conformance Suite.
+ * Copyright 2009, Microsoft Corporation. Distributed under the New BSD License.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   - Neither the name of Microsoft nor the names of its contributors may be
+ *     used to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+*/
+exports["Test ECMAScript 5 Conformance"] = function () {
+  var value = { "a1": { "b1": [1, 2, 3, 4], "b2": { "c1": 1, "c2": 2 } }, "a2": "a2" };
+  
+  // Ensures that a `TypeError` is thrown if an object contains a circular
+  // reference.
+  function assertCyclicError(exception) {
+    return exception.name == "TypeError" && exception.message == "Cyclic structures cannot be serialized.";
+  }
+  
+  // Section 15.12.2: `JSON.parse()`.
+  // --------------------------------
+  
+  // Test 15.12.2-0-1 and 15.12.2-0-2. 15.12.2-0-3 is inapplicable to ES 3.
+  assert.equal("function", typeof prim.parse, "`Prim.parse` should be a function");
+  assert.equal(2, prim.parse.length, "`Prim.parse` should accept two arguments");
+  
+  // Section 15.12.1.1: The JSON Grammar.
+  // ------------------------------------
+  
+  // Tests 15.12.1.1-0-1 thru 15.12.1.1-0-8.
+  exports.parseError("12\t\r\n 34", "Valid whitespace characters may not separate two discrete tokens");
+  exports.parseError("\u000b1234", "The vertical tab is not a valid whitespace character");
+  exports.parseError("\u000c1234", "The form feed is not a valid whitespace character");
+  exports.parseError("\u00a01234", "The non-breaking space is not a valid whitespace character");
+  exports.parseError("\u200b1234", "The zero-width space is not a valid whitespace character");
+  exports.parseError("\ufeff1234", "The byte order mark (zero-width non-breaking space) is not a valid whitespace character");
+  exports.parseError("\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u30001234", "Other Unicode category `Z` characters are not valid whitespace characters");
+  exports.parseError("\u2028\u20291234", "The line (U+2028) and paragraph (U+2029) separators are not valid whitespace characters");
+  
+  // Test 15.12.1.1-0-9.
+  exports.parses({ "property": {}, "prop2": [true, null, 123.456] },
+    '\t\r \n{\t\r \n' +
+    '"property"\t\r \n:\t\r \n{\t\r \n}\t\r \n,\t\r \n' +
+    '"prop2"\t\r \n:\t\r \n' +
+      '[\t\r \ntrue\t\r \n,\t\r \nnull\t\r \n,123.456\t\r \n]' +
+    '\t\r \n}\t\r \n',
+  "Valid whitespace characters may precede and follow all tokens");
+  
+  // Tests 15.12.1.1-g1-1 thru 15.12.1.1-g1-4.
+  exports.parses(1234, "\t1234", "Leading tab characters should be ignored");
+  exports.parseError("12\t34", "A tab character may not separate two disparate tokens");
+  exports.parses(1234, "\r1234", "Leading carriage returns should be ignored");
+  exports.parseError("12\r34", "A carriage return may not separate two disparate tokens");
+  exports.parses(1234, "\n1234", "Leading line feeds should be ignored");
+  exports.parseError("12\n34", "A line feed may not separate two disparate tokens");
+  exports.parses(1234, " 1234", "Leading space characters should be ignored");
+  exports.parseError("12 34", "A space character may not separate two disparate tokens");
+  
+  // Tests 15.12.1.1-g2-1 thru 15.12.1.1-g2-5.
+  exports.parses("abc", '"abc"', "Strings must be enclosed in double quotes");
+  exports.parseError("'abc'", "Single-quoted strings are not permitted");
+  // Note: the original test 15.12.1.1-g2-3 (`"\u0022abc\u0022"`) is incorrect,
+  // as the JavaScript interpreter will always convert `\u0022` to `"`.
+  exports.parseError("\\u0022abc\\u0022", "Unicode-escaped double quote delimiters are not permitted");
+  exports.parseError('"ab'+"c'", "Strings must terminate with a double quote character");
+  exports.parses("", '""', "Strings may be empty");
+  
+  // Tests 15.12.1.1-g4-1 thru 15.12.1.1-g4-4.
+  exports.parseError('"\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007"', "Unescaped control characters in the range [U+0000, U+0007] are not permitted within strings");
+  exports.parseError('"\u0008\u0009\u000a\u000b\u000c\u000d\u000e\u000f"', "Unescaped control characters in the range [U+0008, U+000F] are not permitted within strings");
+  exports.parseError('"\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017"', "Unescaped control characters in the range [U+0010, U+0017] are not permitted within strings");
+  exports.parseError('"\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f"', "Unescaped control characters in the range [U+0018, U+001F] are not permitted within strings");
+  
+  // Tests 15.12.1.1-g5-1 thru 15.12.1.1-g5-3.
+  exports.parses("X", '"\\u0058"', "Unicode escape sequences are permitted within strings");
+  exports.parseError('"\\u005"', "Unicode escape sequences may not comprise fewer than four hexdigits");
+  exports.parseError('"\\u0X50"', "Unicode escape sequences may not contain non-hex characters");
+  
+  // Tests 15.12.1.1-g6-1 thru 15.12.1.1-g6-7.
+  exports.parses("/", '"\\/"', "Escaped solidus");
+  exports.parses("\\", '"\\\\"', "Escaped reverse solidus");
+  exports.parses("\b", '"\\b"', "Escaped backspace");
+  exports.parses("\f", '"\\f"', "Escaped form feed");
+  exports.parses("\n", '"\\n"', "Escaped line feed");
+  exports.parses("\r", '"\\r"', "Escaped carriage return");
+  exports.parses("\t", '"\\t"', "Escaped tab");
+  
+  // Section 15.12.3: `JSON.stringify()`.
+  // ------------------------------------
+  
+  // Test 15.12.3-0-1 and 15.12.3-0-2. 15.12.3-0-3 is inapplicable.
+  assert.equal("function", typeof prim.stringify, "`Prim.stringify` should be a function");
+  assert.equal(3, prim.stringify.length, "`Prim.stringify` should accept three arguments");
+  
+  // Test 15.12.3-11-1 thru 5.12.3-11-15.
+  exports.serializes(void 0, void 0, "`JSON.stringify(undefined)` should return `undefined`");
+  exports.serializes('"replacement"', void 0, "The `JSON.stringify` callback function can be called on a top-level `undefined` value", function (key, value) {
+    return "replacement";
+  });
+  exports.serializes('"a string"', "a string", "`JSON.stringify` should serialize top-level string primitives");
+  exports.serializes("123", 123, "`JSON.stringify` should serialize top-level number primitives");
+  exports.serializes("true", true, "`JSON.stringify` should serialize top-level Boolean primitives");
+  exports.serializes("null", null, "`JSON.stringify` should serialize top-level `null` values");
+  exports.serializes("42", new Number(42), "`JSON.stringify` should serialize top-level number objects");
+  exports.serializes('"wrapped"', new String("wrapped"), "`JSON.stringify` should serialize top-level string objects");
+  exports.serializes("false", new Boolean(false), "`JSON.stringify` should serialize top-level Boolean objects");
+  exports.serializes(void 0, 42, "The `JSON.stringify` callback function may return `undefined` when called on a top-level number primitive", function () {
+    return void 0;
+  });
+  exports.serializes(void 0, { "prop": 1 }, "The `JSON.stringify` callback function may return `undefined` when called on a top-level object", function () {
+    return void 0;
+  });
+  exports.serializes("[4,2]", 42, "The `JSON.stringify` callback function may return an array when called on a top-level number primitive", function (key, value) {
+    return value == 42 ? [4, 2] : value;
+  });
+  exports.serializes('{"forty":2}', 42, "The `JSON.stringify` callback function may return an object literal when called on a top-level number primitive", function (key, value) {
+    return value == 42 ? { "forty": 2 } : value;
+  });
+  exports.serializes(void 0, function () {}, "`JSON.stringify` should return `undefined` when called on a top-level function");
+  exports.serializes("99", function () {}, "The `JSON.stringify` callback function may return a number primitive when called on a top-level function", function () {
+    return 99;
+  });
+  
+  // Test 15.12.3-4-1.
+  exports.serializes("[42]", [42], "`JSON.stringify` should ignore `filter` arguments that are not functions or arrays", {});
+  
+  // Test 15.12.3-5-a-i-1 and 15.12.3-5-b-i-1.
+  assert.equal(prim.stringify(value, null, new Number(5)), prim.stringify(value, null, 5), "Optional `width` argument: Number object and primitive width values should produce identical results");
+  assert.equal(prim.stringify(value, null, new String("xxx")), prim.stringify(value, null, "xxx"), "Optional `width` argument: String object and primitive width values should produce identical results");
+  
+  // Test 15.12.3-6-a-1 and 15.12.3-6-a-2.
+  assert.equal(prim.stringify(value, null, 10), prim.stringify(value, null, 100), "Optional `width` argument: The maximum numeric width value should be 10");
+  assert.equal(prim.stringify(value, null, 5.99999), prim.stringify(value, null, 5), "Optional `width` argument: Numeric values should be converted to integers");
+  
+  // Test 15.12.3-6-b-1 and 15.12.3-6-b-4.
+  assert.equal(prim.stringify(value, null, 0.999999), prim.stringify(value), "Optional `width` argument: Numeric width values between 0 and 1 should be ignored");
+  assert.equal(prim.stringify(value, null, 0), prim.stringify(value), "Optional `width` argument: Zero should be ignored");
+  assert.equal(prim.stringify(value, null, -5), prim.stringify(value), "Optional `width` argument: Negative numeric values should be ignored");
+  assert.equal(prim.stringify(value, null, 5), prim.stringify(value, null, "     "), "Optional `width` argument: Numeric width values in the range [1, 10] should produce identical results to that of string values containing `width` spaces");
+  
+  // Test 15.12.3-7-a-1.
+  assert.equal(prim.stringify(value, null, "0123456789xxxxxxxxx"), prim.stringify(value, null, "0123456789"), "Optional `width` argument: String width values longer than 10 characters should be truncated");
+  
+  // Test 15.12.3-8-a-1 thru 15.12.3-8-a-5.
+  assert.equal(prim.stringify(value, null, ""), prim.stringify(value), "Empty string `width` arguments should be ignored");
+  assert.equal(prim.stringify(value, null, true), prim.stringify(value), "Boolean primitive `width` arguments should be ignored");
+  assert.equal(prim.stringify(value, null, null), prim.stringify(value), "`null` `width` arguments should be ignored");
+  assert.equal(prim.stringify(value, null, new Boolean(false)), prim.stringify(value), "Boolean object `width` arguments should be ignored");
+  assert.equal(prim.stringify(value, null, value), prim.stringify(value), "Object literal `width` arguments should be ignored");
+  
+  // Test 15.12.3@2-2-b-i-1.
+  exports.serializes('["fortytwo objects"]', [{
+    "prop": 42,
+    "toJSON": function () {
+      return "fortytwo objects";
+    }
+  }], "An object literal with a custom `toJSON` method nested within an array may return a string primitive for serialization");
+  
+  // Test 15.12.3@2-2-b-i-2.
+  exports.serializes('[42]', [{
+    "prop": 42,
+    "toJSON": function () {
+      return new Number(42);
+    }
+  }], "An object literal with a custom `toJSON` method nested within an array may return a number object for serialization");
+  
+  // Test 15.12.3@2-2-b-i-3.
+  exports.serializes('[true]', [{
+    "prop": 42,
+    "toJSON": function () {
+      return new Boolean(true);
+    }
+  }], "An object liyeral with a custom `toJSON` method nested within an array may return a Boolean object for serialization");
+  
+  // Test 15.12.3@2-3-a-1.
+  exports.serializes('["fortytwo"]', [42], "The `JSON.stringify` callback function may return a string object when called on an array", function (key, value) {
+    return value === 42 ? new String("fortytwo") : value;
+  });
+  
+  // Test 15.12.3@2-3-a-2.
+  exports.serializes('[84]', [42], "The `JSON.stringify` callback function may return a number object when called on an array", function (key, value) {
+    return value === 42 ? new Number(84) : value;
+  });
+  
+  // Test 15.12.3@2-3-a-3.
+  exports.serializes('[false]', [42], "The `JSON.stringify` callback function may return a Boolean object when called on an array", function (key, value) {
+    return value === 42 ? new Boolean(false) : value;
+  });
+  
+  // Test 15.12.3@4-1-2. 15.12.3@4-1-1 only tests whether an exception is
+  // thrown; the type of the exception is not checked.
+  value = {};
+  value.prop = value;
+  assert.throws(function () {
+    prim.stringify(value);
+  }, assertCyclicError, "An object containing a circular reference should throw a `TypeError`");
+  
+  // Test 15.12.3@4-1-3, modified to ensure that a `TypeError` is thrown.
+  value = {p1: {p2: {}}};
+  value.p1.p2.prop = value;
+  assert.throws(function () {
+    prim.stringify(value);
+  }, assertCyclicError, "A cyclic structure should throw a `TypeError`");
 };
 
 // Run the unit tests.
